252b1452542a83def06990e43e7961f1661e6978
LanPenn <119684139+LanPenn@users.noreply.github.com>
2024-03-19 15:16:47 +0800

    Update dag2file.go

--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,23 @@
 package merkledag
 
-// Hash to file
+// Hash2File 将哈希对应的数据从 KVStore 中读取出来，然后根据路径返回对应的文件内容。
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
-	return nil
+	// 获取哈希函数实例
+	hashFunc := hp.Get()
+
+	// 在哈希函数中写入路径
+	hashFunc.Write([]byte(path))
+	// 计算路径的哈希值
+	pathHash := hashFunc.Sum(nil)
+
+	// 检查传入的哈希是否与路径的哈希相等，如果不等，返回空
+	if !bytes.Equal(hash, pathHash) {
+		return nil
+	}
+
+	// 从 KVStore 中检索数据
+	data := store.Get(hash)
+
+	// 返回数据（文件内容）
+	return data
 }
